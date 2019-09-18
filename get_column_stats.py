96f4fb009c95ce08f18277617e3ee60260737c41
Ellen Waddle <elwa8931@colorado.edu>
2019-09-18 15:47:56 -0600

    edited to make sure that stdev fct was included

--- get_column_stats.py
+++ get_column_stats.py
@@ -2,30 +2,26 @@ import sys
 import math
 import argparse
 
+parser = argparse.ArgumentParser(description='supply file and colnum')
 
-parser = argparse.ArgumentParser(description='supply numbers', prog='bay')
+parser.add_argument('--file_name', type=str, help='your file',required=True)
 
-parser.add_argument('​--file_name​', ​type​=​str​,
-                    help​=​'Name of the file'​, required=​True​)
-
-parser.add_argument(​'--column_number'​, ​type​=​int​,
-                    help​=​'The column number'​, required=​True​)
+parser.add_argument('--column_number', type=int, help='number of columns',
+                    required=True)
 
 args = parser.parse_args()
+print(args.file_name, args.column_number)
 
-file_name = sys.argv[1]
-col_num = int(sys.argv[2])
+f=open(file_name,'r')
 
-try:
-    f = open(file_name, 'r')
-except FileNotFoundError:
-    print('Could not locate' + file_name)
-    sys.exit(1)
-except​ ​PermissionError​:
-    print​(​'Could not open '​ + file_name)
-    sys.exit(1)
-​finally​:
-    return​ f
+#try:
+#    f = open(file_name) #this needs to be fixed to be generic
+#except FileNotFoundError:
+#    print('Could not locate' + file_name)
+#    sys.exit(1)
+#except PermissionError:
+#    print('Could not access' + file_name)
+#    sys.exit(1)
 
 V = []
 
@@ -34,8 +30,10 @@ for l in f:
     V.append(A[col_num])
 
 mean = sum(V)/len(V)
+return mean
 
 stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+return stdev
 
 print('mean:', mean)
 print('stdev:', stdev)
