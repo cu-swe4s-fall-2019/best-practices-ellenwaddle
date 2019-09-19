f827e5559a33e25850df31e004c20aaef17cc77b
Ellen Waddle <elwa8931@colorado.edu>
2019-09-19 12:57:51 -0600

    created file for unit tests

---
+++ basics_test.py
@@ -0,0 +1,22 @@
+#unit tests for mean and stdev
+#add unit tests for mean and stdev, include randomness. test for exceptions.
+#hint: these methods should raise exceptions.
+
+
+import unittest
+import get_column_stats
+import random
+import os
+
+class TestGetColumnStats(unittest.TestCase):
+    def test_whatever(self):
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
